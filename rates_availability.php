79f26f3380b7c8b5181c4a0e7905f621df1a7b88
Junrybuenavista <42229618+Junrybuenavista@users.noreply.github.com>
2021-11-27 14:48:38 +0800

    Update rates_availability.php

--- rates_availability.php
+++ rates_availability.php
@@ -5,12 +5,14 @@
 		$username = "htgrp";
 		$password = "84deb21061cef5be6e19c8876967a02b";
 		//room type 30192 30191 30190
-		//rate plan 34045,34044,34043
+		//rate plan 34045 34044 34043
+		//room id   474404 474403
 		curl_setopt_array($curl, array(
 			  //CURLOPT_URL => 'https://sky-us2.clock-software.com/pms_api/90805/12612/room_types/',
 			  //CURLOPT_URL => 'https://sky-us2.clock-software.com/pms_api/90805/12612/rooms/',
-			  //CURLOPT_URL => 'https://sky-us2.clock-software.com/pms_api/90805/12612/rate_plans/',
-			  CURLOPT_URL => 'https://sky-us2.clock-software.com/pms_api/90805/12612/rates_availability/?from=2021-11-01&to=2021-11-30&rates=34043&room_types=:30190/',
+			  //CURLOPT_URL => 'https://sky-us2.clock-software.com/pms_api/90805/12612/rates/',
+			  CURLOPT_URL => 'https://sky-us2.clock-software.com/pms_api/90805/12612/rates_availability/?from=2021-11-01&to=2021-11-30&rates=364890&room_types=30191',
+			  //CURLOPT_URL => 'https://sky-us2.clock-software.com/pms_api/90805/12612/rates_availability?from=2021-11-01&to=2021-11-30&rates%5B%5D=364891&room_types%5B%5D=30191',
 			  CURLOPT_RETURNTRANSFER => true,
 			  CURLOPT_ENCODING => '',
 			  CURLOPT_MAXREDIRS => 10,
@@ -27,3 +29,10 @@
 
 		curl_close($curl);
 		echo $response;
+		echo "<br><br><br>";
+
+$json = $response;
+
+$arr = json_decode($json, TRUE);
+
+print_r($arr);
